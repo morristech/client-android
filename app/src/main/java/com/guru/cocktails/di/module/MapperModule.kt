05fef6f863b88f32eefc8495c07c7005aaa724af
Michal Demcak <michaldemcak@Michals-MacBook-Pro.local>
2018-02-17 22:43:21 +0100

    Injection for ingredients mapper

--- app/src/main/java/com/guru/cocktails/di/module/MapperModule.kt
+++ app/src/main/java/com/guru/cocktails/di/module/MapperModule.kt
@@ -1,11 +1,17 @@
 package com.guru.cocktails.di.module
 
+import com.guru.cocktails.data.mapper.IngredientTypeMapper
+import com.guru.cocktails.data.mapper.IngredientDetailMapper
 import com.guru.cocktails.data.mapper.WeatherEntityMapper
 import com.guru.cocktails.data.mapper.WeatherMapper
 import com.guru.cocktails.data.mapper.base.Mapper
 import com.guru.cocktails.data.source.local.model.WeatherEntity
 import com.guru.cocktails.data.source.remote.model.WeatherDto
+import com.guru.cocktails.data.source.remote.model.ingredientDto.IngredientDetailDto
+import com.guru.cocktails.data.source.remote.model.ingredientDto.IngredientTypeDto
 import com.guru.cocktails.domain.model.Weather
+import com.guru.cocktails.domain.model.ingredient.IngredientDetail
+import com.guru.cocktails.domain.model.ingredient.IngredientType
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -25,4 +31,15 @@ class MapperModule {
         return mapper
     }
 
+    @Singleton
+    @Provides
+    internal fun ingredientDetailMapper(mapper: IngredientDetailMapper): Mapper<IngredientDetail, IngredientDetailDto> {
+        return mapper
+    }
+
+    @Singleton
+    @Provides
+    internal fun ingredientTypeMapper(mapper: IngredientTypeMapper): Mapper<IngredientType, IngredientTypeDto> {
+        return mapper
+    }
 }
