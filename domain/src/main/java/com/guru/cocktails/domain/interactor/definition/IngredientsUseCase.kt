feb235352cbc913601b3db249746a8dead5f4319
Martin Petrulak <mato.petrulak@gmail.com>
2018-03-08 17:18:58 +0100

    Adding refresh methods into use case.

--- domain/src/main/java/com/guru/cocktails/domain/interactor/definition/IngredientsUseCase.kt
+++ domain/src/main/java/com/guru/cocktails/domain/interactor/definition/IngredientsUseCase.kt
@@ -2,13 +2,18 @@ package com.guru.cocktails.domain.interactor.definition
 
 import com.guru.cocktails.domain.model.ingredient.IngredientDetail
 import com.guru.cocktails.domain.model.ingredient.IngredientThumb
+import io.reactivex.Completable
 import io.reactivex.Flowable
 
 interface IngredientsUseCase {
 
     fun getAllAlcoholicIngredients(): Flowable<List<IngredientThumb>>
 
+    fun refreshAllAlcoholicIngredients(): Completable
+
     fun getAllNonAlcoholicIngredients(): Flowable<List<IngredientThumb>>
+
+    fun refreshAllNonAlcoholicIngredients(): Completable
 
     fun getIngredientDetail(ingredientId: Int): Flowable<IngredientDetail>
 }
